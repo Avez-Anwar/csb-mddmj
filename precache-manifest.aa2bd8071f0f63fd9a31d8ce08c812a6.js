eb6262fa66e900e680d27249d8f3269a8d9ba9b9
Code Sandbox <hello@codesandbox.io>
2021-03-15 14:39:20 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.aa2bd8071f0f63fd9a31d8ce08c812a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "48acb6f85f1d18a2d302",
+    "url": "https://Avez-Anwar.github.io/csb-mddmj/static/js/main.48acb6f8.chunk.js"
+  },
+  {
+    "revision": "d9a1ec7fc128b68f6d9a",
+    "url": "https://Avez-Anwar.github.io/csb-mddmj/static/js/1.d9a1ec7f.chunk.js"
+  },
+  {
+    "revision": "8c7183456c00faf5db0c",
+    "url": "https://Avez-Anwar.github.io/csb-mddmj/static/js/runtime~main.8c718345.js"
+  },
+  {
+    "revision": "372a710e14494510b9a817dbef051225",
+    "url": "https://Avez-Anwar.github.io/csb-mddmj/index.html"
+  }
+];
---
+++ static/js/main.48acb6f8.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,t){e.exports=t(4)},function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o),l=t(2),a=t.n(l);function c(){return r.a.createElement("h1",null,"Hello, World!")}var u=document.getElementById("root");a.a.render(r.a.createElement(c,null),u)}],[[3,2,1]]]);
+//# sourceMappingURL=main.48acb6f8.chunk.js.map
